6465bcf3ad08463b8919f092d6b7c55214d205df
Fady Mondy <EngFadyMondy@gmail.com>
2023-02-07 18:19:43 +0200

    first commit 🔥

---
+++ README.md
@@ -0,0 +1,64 @@
+# Tomato media
+
+Media Libarary UI to select files on input or browse your media files
+
+## Installation
+
+```bash
+composer require tomatophp/tomato-media
+```
+after install your package please run this command
+
+```bash
+php artisan tomato-media:install
+```
+
+## Publish Assets
+
+you can publish config file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-media-config"
+```
+
+you can publish views file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-media-views"
+```
+
+you can publish languages file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-media-lang"
+```
+
+you can publish migrations file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-media-migrations"
+```
+
+## Support
+
+you can join our discord server to get support [TomatoPHP](https://discord.gg/Xqmt35Uh)
+
+## Docs
+
+you can check docs of this package on [Docs](https://docs.tomatophp.com/plugins/laravel-package-generator)
+
+## Changelog
+
+Please see [CHANGELOG](CHANGELOG.md) for more information on what has changed recently.
+
+## Security
+
+Please see [SECURITY](SECURITY.md) for more information about security.
+
+## Credits
+
+- [Tomatophp](mailto:info@3x1.io)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/TomatoMediaServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tomatophp\TomatoMedia;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class TomatoMediaServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //Register generate command
+        $this->commands([
+           \Tomatophp\TomatoMedia\Console\TomatoMediaInstall::class,
+        ]);
+ 
+        //Register Config file
+        $this->mergeConfigFrom(__DIR__.'/../config/tomato-media.php', 'tomato-media');
+ 
+        //Publish Config
+        $this->publishes([
+           __DIR__.'/../config/tomato-media.php' => config_path('tomato-media.php'),
+        ], 'tomato-media-config');
+ 
+        //Register Migrations
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+ 
+        //Publish Migrations
+        $this->publishes([
+           __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'tomato-media-migrations');
+        //Register views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'tomato-media');
+ 
+        //Publish Views
+        $this->publishes([
+           __DIR__.'/../resources/views' => resource_path('views/vendor/tomato-media'),
+        ], 'tomato-media-views');
+ 
+        //Register Langs
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'tomato-media');
+ 
+        //Publish Lang
+        $this->publishes([
+           __DIR__.'/../resources/lang' => app_path('lang/vendor/tomato-media'),
+        ], 'tomato-media-lang');
+ 
+        //Register Routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+ 
+    }
+
+    public function boot(): void
+    {
+        //you boot methods here
+    }
+}
